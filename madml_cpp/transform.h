050d970cefe0f3ac7817c01e3e2e8a4224769b20
monishramadoss <monish.ramadoss@outlook.com>
2020-05-17 01:28:27 -0700

    adding files

---
+++ madml_cpp/transform.h
@@ -0,0 +1,16 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include <vector>
+#include "madml.h"
+#include "layer.h"
+
+namespace kernel {
+	namespace layers {
+		
+	}
+}
+
+
+
+#endif //!TRANSFORM_H
